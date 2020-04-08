e28455ae3c0c825573908ee4bcc233f036e08a97
Mae Kangsadan <62905988+palioudis@users.noreply.github.com>
2020-04-08 13:54:05 +0200

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# JSsample
-Created with CodeSandbox
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Hello Vanilla!</h1>
+<div>
+  We use Parcel to bundle this sandbox, you can find more info about Parcel
+  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+</div>
+`;
+
+function multiply(x, y = 1) {
+  return x * y;
+}
+
+console.log(multiply(4, 5));
